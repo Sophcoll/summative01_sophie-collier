5a2902eac25db477272d02f2b0ee69d62a3fa87b
Sophcoll <107028500+Sophcoll@users.noreply.github.com>
2022-09-19 12:06:30 +1200

    updates

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// THIS FUNCTION TOGGLES THE LANGUAGE DROP DOWN MENU ON AND OFF
+
+function toggleClass(){
+    var menuElement=document.querySelector(".dropdown-menu");
+
+    menuElement.classList.toggle("hide")
+}
+
